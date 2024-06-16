61d50195d2c68f3c9e44cc38f068def886538884
Ares VRC <aresvrcofficial@gmail.com>
2024-06-16 18:14:25 -0500

    Add all website files

---
+++ start.bat
@@ -0,0 +1,7 @@
+echo "# park-manager-beyond-the-gates" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AresVRCOfficial/park-manager-beyond-the-gates.git
+git push -u origin main
